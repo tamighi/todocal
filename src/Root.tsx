ec7fd737bbd2375d0e753b3217db7318067c77cf
tamighi <tamighi@hotmail.com>
2023-11-19 14:27:19 +0100

    feat(routing): add basic config

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,22 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { MainScreen } from "./screens";
+
+const Stack = createNativeStackNavigator();
 
 const Root = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default Root;
