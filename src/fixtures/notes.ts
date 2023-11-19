de1b9373bc84db8827bc79dea6e62879491363e6
tamighi <tamighi@hotmail.com>
2023-11-19 17:08:56 +0100

    feat(data): add Note model + fixtures

---
+++ src/fixtures/notes.ts
@@ -0,0 +1,25 @@
+import { LoremIpsum } from "lorem-ipsum";
+import { Note } from "../models";
+
+const NOTES_FIXTURES: Array<Note> = [];
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+for (let i = 0; i < 100; i++) {
+  NOTES_FIXTURES.push({
+    id: i.toString(),
+    title: lorem.generateWords(Math.round(Math.random() * 10) + 2),
+    body: lorem.generateSentences(Math.round(Math.random() * 3) + 1),
+  });
+}
+
+export default NOTES_FIXTURES;
