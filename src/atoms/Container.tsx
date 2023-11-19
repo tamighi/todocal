cd77b88995c1aba4441c03211fd6e689671a387e
tamighi <tamighi@hotmail.com>
2023-11-19 18:54:05 +0100

    fix(atoms): container type

--- src/atoms/Container.tsx
+++ src/atoms/Container.tsx
@@ -1,10 +1,6 @@
-import { Theme } from "../themes";
-import { BoxProps } from "@shopify/restyle";
-import Box from "./Box";
+import Box, { BoxProps } from "./Box";
 
-const Container: React.FC<React.PropsWithChildren<BoxProps<Theme>>> = (
-  props,
-) => (
+const Container: React.FC<BoxProps> = (props) => (
   <Box {...props} flex={1} backgroundColor="$background">
     {props.children}
   </Box>
