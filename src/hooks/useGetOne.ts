c344a3be0d9572ac324f0a301a3099c63311ed1d
tamighi <tamighi@hotmail.com>
2023-12-23 17:19:43 +0100

    refactor: useGetOne simplified

--- src/hooks/useGetOne.ts
+++ src/hooks/useGetOne.ts
@@ -1,24 +1,12 @@
-import React from "react";
-
 import { Resource, ResourceTypes, serviceMap } from "@/services";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 export const useGetOne = <R extends Resource>(resource: R, id: string) => {
-  const queryClient = useQueryClient();
-
-  const initialData = React.useCallback(() => {
-    const data = queryClient.getQueryData<ResourceTypes[R][]>([resource]);
-
-    const initialData = data?.find((item) => item.id === id);
-    return initialData;
-  }, []);
-
   const queryResult = useQuery<ResourceTypes[R]>({
     queryKey: [resource, id],
     queryFn: (() => serviceMap[resource].getOne(id)) as () => Promise<
       ResourceTypes[R]
     >,
-    initialData,
   });
 
   return queryResult;
