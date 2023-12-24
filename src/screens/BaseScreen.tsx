5366fa16a0eda3ddab923dd7e881443849a4c081
tamighi <tamighi@hotmail.com>
2023-12-24 17:06:58 +0100

    feat: add header with settings

--- src/screens/BaseScreen.tsx
+++ src/screens/BaseScreen.tsx
@@ -1,11 +1,16 @@
-import { Container, ContainerProps } from "@/atoms";
-import { SafeAreaView } from "react-native";
+import { Box, Text, Container, ContainerProps } from "@/atoms";
+import { Pressable, SafeAreaView } from "react-native";
 
 type Props = ContainerProps;
 
 export const BaseScreen: React.FC<Props> = ({ children, ...rest }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <Box alignSelf="flex-end">
+        <Pressable>
+          <Text>Settings</Text>
+        </Pressable>
+      </Box>
       <Container {...rest}>{children}</Container>
     </SafeAreaView>
   );
