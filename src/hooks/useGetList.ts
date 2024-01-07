1a3805ffc6cd36d8c5659814c2a4e46567689802
tamighi <tamighi@hotmail.com>
2024-01-07 11:22:44 +0100

    fix: type useGetList

--- src/hooks/useGetList.ts
+++ src/hooks/useGetList.ts
@@ -1,15 +1,14 @@
-import { Resource, serviceMap } from "@/services";
+import { Resource, ResourceTypes, serviceMap } from "@/services";
 import { useQuery } from "@tanstack/react-query";
 
-type ServiceGetListFns = {
-  [K in keyof typeof serviceMap]: (typeof serviceMap)[K]["getList"];
-};
+type ServiceGetListFns<T extends keyof typeof serviceMap> =
+  (typeof serviceMap)[T]["getList"];
 
 export const useGetList = <R extends Resource>(
   resource: R,
-  query?: Parameters<ServiceGetListFns[R]>[0],
+  query?: Parameters<ServiceGetListFns<R>>[0],
 ) => {
-  const queryResult = useQuery({
+  const queryResult = useQuery<ResourceTypes[R][], Error, ResourceTypes[R][]>({
     queryKey: [resource, query],
     //@ts-expect-error Idk how to match the fn to it's argument
     queryFn: () => serviceMap[resource].getList(query),
