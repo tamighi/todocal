3e17f3e3df7af2e7d0c461d2a1dd632977161428
tamighi <tamighi@hotmail.com>
2024-01-13 12:32:43 +0100

    feat: add taglist

--- src/components/core/settings/SettingsTagList.tsx
+++ src/components/core/settings/SettingsTagList.tsx
@@ -1,9 +1,31 @@
-import { Box, Text } from "@/atoms";
+import { Box, Chip, Text } from "@/atoms";
+import { useGetList, useTheme } from "@/hooks";
 
 export const SettingsTagList = () => {
+  const { data: tags } = useGetList("tag");
+  const theme = useTheme();
+
   return (
     <Box>
-      <Text>Hello world</Text>
+      {tags && tags.length > 0 ? (
+        <Box gap="xs">
+          {tags.map((tag) => {
+            return (
+              <Chip
+                key={tag.id}
+                style={{
+                  backgroundColor:
+                    tag.color || theme.colors.chipDefaultBackground,
+                }}
+              >
+                <Text>{tag.name}</Text>
+              </Chip>
+            );
+          })}
+        </Box>
+      ) : (
+        <Text>No tags yet</Text>
+      )}
     </Box>
   );
 };
