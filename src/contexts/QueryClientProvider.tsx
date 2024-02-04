ebb14a3cd4593eb50b201c9f3a836a2d663aec0e
tamighi <tomi-lys@live.fr>
2024-02-04 09:55:52 +0100

    feat: new mutation cache & query cache

--- src/contexts/QueryClientProvider.tsx
+++ src/contexts/QueryClientProvider.tsx
@@ -1,25 +1,41 @@
 import {
+  MutationCache,
+  QueryCache,
   QueryClient,
   QueryClientProvider as RNQueryClientProvider,
 } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      retry: false,
-    },
-    mutations: {
-      retry: false,
-    },
-  },
-});
 
 export const QueryClientProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const queryCache = new QueryCache({
+    onError: (e) => {
+      console.log(e);
+    },
+  });
+
+  const mutationCache = new MutationCache({
+    onError: (e) => {
+      console.log(e);
+    },
+  });
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        retry: false,
+      },
+      mutations: {
+        retry: false,
+      },
+    },
+    queryCache,
+    mutationCache,
+  });
+
   return (
     <RNQueryClientProvider client={queryClient}>
       {children}
