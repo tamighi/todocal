6268eb2f87095fd55453c171f3dded60aa254c67
tamighi <tomi-lys@live.fr>
2024-03-03 18:33:16 +0100

    feat(filter): display toggles only if filters

--- src/screens/ScreenHeader.tsx
+++ src/screens/ScreenHeader.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { Box, Button } from "@/atoms";
 import { useTodoFilters } from "@/contexts";
 import { useNavigation } from "@/hooks";
@@ -6,7 +8,7 @@ import { Feather } from "@expo/vector-icons";
 
 export const ScreenHeader = () => {
   const navigation = useNavigation();
-  const { toggleFilters } = useTodoFilters();
+  const { toggleFilters, filters } = useTodoFilters();
 
   const openSettings = () => {
     navigation.navigate("Settings");
@@ -16,9 +18,24 @@ export const ScreenHeader = () => {
     navigation.navigate("Month", { monthId: getCurrentMonthId() });
   };
 
+  const hasFilters = React.useCallback(() => {
+    for (const filter of Object.values(filters)) {
+      if (typeof filter === "object") {
+        if (Object.values(filter).findIndex((v) => v === true) !== -1) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }, [filters]);
+
   return (
     <Box flexDirection="row" justifyContent="flex-end">
-      <Button style={{ margin: 4 }} variant="icon" onPress={toggleFilters}>
+      <Button
+        style={{ margin: 4, display: hasFilters() ? "flex" : "none" }}
+        variant="icon"
+        onPress={toggleFilters}
+      >
         <Feather name="filter" size={22} />
       </Button>
       <Button style={{ margin: 4 }} variant="icon" onPress={navigateToday}>
