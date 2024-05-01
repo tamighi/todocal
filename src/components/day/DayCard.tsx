2e2b17df22a3cd0819abf23886e0a5968150dc29
tamighi <tomi-lys@live.fr>
2024-05-01 16:53:48 +0200

    perf(filters): only change filtered todos if length changed

--- src/components/day/DayCard.tsx
+++ src/components/day/DayCard.tsx
@@ -8,6 +8,7 @@ import { filterTodos } from "@/utils";
 import { DayCardHeader } from "./DayCardHeader";
 import { SimpleTodoList } from "./SimpleTodoList";
 import { DraggableTodoList } from "./DraggableTodoList";
+import { Todo } from "@/models";
 
 type Props = {
   dayId: string;
@@ -16,6 +17,7 @@ type Props = {
 
 export const DayCard: React.FC<Props> = (props) => {
   const { dayId, small = false, ...rest } = props;
+  const [filteredTodos, setFilteredTodos] = React.useState<Todo[]>();
 
   const { data: todos } = useGetList("todo", {
     where: { day: { id: dayId } },
@@ -23,15 +25,26 @@ export const DayCard: React.FC<Props> = (props) => {
 
   const { filters } = useTodoFilters();
 
+  React.useEffect(() => {
+    const newFilteredTodos = filterTodos(todos, filters);
+    if (newFilteredTodos.length !== filteredTodos?.length) {
+      setFilteredTodos(newFilteredTodos);
+    }
+  }, [filters, filterTodos, todos]);
+
+  React.useEffect(() => {
+    setFilteredTodos(filterTodos(todos, filters));
+  }, [todos]);
+
   const date = React.useMemo(() => new Date(dayId), [dayId]);
 
   return (
     <Card borderRadius="s" flex={1} padding="xxs" {...rest}>
       <DayCardHeader dayOnly={small} day={date} />
       {small ? (
-        <SimpleTodoList dayId={dayId} todos={filterTodos(todos, filters)} />
+        <SimpleTodoList dayId={dayId} todos={filteredTodos} />
       ) : (
-        <DraggableTodoList dayId={dayId} todos={filterTodos(todos, filters)} />
+        <DraggableTodoList dayId={dayId} todos={filteredTodos} />
       )}
     </Card>
   );
